a07ebf5468d813a41f18ec3bbf8825f145014830
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-30 14:22:37 +0300

    refactor: Update MissionsPage component to use MissionCard and BackgroundBeams

--- app/missions/page.tsx
+++ app/missions/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import MissionCard from "../components/missions/MissionCard";
+import { BackgroundBeams } from "../components/ui/background-beams";
+import mockMissionData from "../data/missions";
 const MissionsPage = () => {
   return (
-    <div>MissionsPage</div>
-  )
-}
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 p-4">
+      {mockMissionData.map((mission, index) => (
+        <MissionCard
+          key={index}
+          title={mission.title}
+          description={mission.description}
+          points={mission.points}
+        />
+      ))}
+      <BackgroundBeams />
+    </div>
+  );
+};
 
-export default MissionsPage
+export default MissionsPage;
