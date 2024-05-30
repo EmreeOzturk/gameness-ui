83e462d7e0078fb24e330015ff2dd03b8ca3bfed
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-31 02:11:51 +0300

    refactor: Add MissionSection component and update EpoachTab to display mission sections

--- app/components/tabs/EpoachTab.tsx
+++ app/components/tabs/EpoachTab.tsx
@@ -1,3 +1,5 @@
+import MissionSection from "./MissionSection";
+
 type EpoachTabProps = {
   title: string;
   description: string;
@@ -12,6 +14,10 @@ const EpoachTab: React.FC<EpoachTabProps> = ({
   return (
     <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-zinc-900 to-zinc-950">
       <p>{title}</p>
+      <div className="flex items-center h-full  justify-between">
+        <MissionSection title="Daily Missions" />
+        <MissionSection title="Weekly Missions" />
+      </div>
     </div>
   );
 };
