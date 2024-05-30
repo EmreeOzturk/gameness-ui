c2d73f1080a3ed8961d1dc04521a63c178062126
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-31 02:45:05 +0300

    refactor: Update Navbar component to use new icon for DATS Project

--- app/components/header/Navbar.tsx
+++ app/components/header/Navbar.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
-import { Xicon, DiscordIcon, LogoIcon } from "../icons";
-
+import { Xicon, DiscordIcon } from "../icons";
+import Image from "next/image";
 const navItems = [
   {
-    name: "Gameness",
+    name: "DATS Project",
     link: "/",
-    icon: <LogoIcon />,
+    iconUri: "/mavi3.svg",
   },
   {
     name: "Twitter",
@@ -24,11 +24,19 @@ const Navbar = () => {
     <div className="flex items-center justify-center gap-4 z-20 ">
       {navItems.map((item) => (
         <Link
-          className="text-zinc-300 flex items-center justify-center gap-2 text-md rounded-xl px-2 transition-all py-2 hover:text-red-300"
+          className="text-zinc-300 flex items-center justify-center gap-2 text-md rounded-xl px-2 transition-all py-2 hover:text-cyan-300"
           href={item.link}
           key={item.name}
         >
-          {item.icon}
+          {item.iconUri && (
+            <Image
+              src={item.iconUri!}
+              alt="Picture of the author"
+              width={21}
+              height={21}
+            />
+          )}
+          {item.icon && item.icon}
           {item.name}
         </Link>
       ))}
