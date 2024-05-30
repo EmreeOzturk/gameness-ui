2b0c1d90fbf6dbca8a11d4ba6bd5be67e5729c4a
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-30 13:54:55 +0300

    feat: Update Navbar component in header with icons

--- app/components/header/Navbar.tsx
+++ app/components/header/Navbar.tsx
@@ -1,25 +1,34 @@
 import Link from "next/link";
+import { Xicon, DiscordIcon, LogoIcon } from "../icons";
 
 const navItems = [
   {
     name: "Gameness",
     link: "/",
+    icon: <LogoIcon />,
   },
   {
     name: "Twitter",
     link: "/",
+    icon: <Xicon />,
   },
   {
     name: "Discord",
     link: "/",
+    icon: <DiscordIcon />,
   },
 ];
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-center gap-4">
+    <div className="flex items-center justify-center gap-4 z-20 ">
       {navItems.map((item) => (
-        <Link href={item.link} key={item.name}>
+        <Link
+          className="text-zinc-300 flex items-center justify-center gap-2 text-md rounded-xl px-2 transition-all py-2 hover:text-red-300"
+          href={item.link}
+          key={item.name}
+        >
+          {item.icon}
           {item.name}
         </Link>
       ))}
