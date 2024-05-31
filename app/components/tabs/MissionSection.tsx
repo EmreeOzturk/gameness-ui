3f19c380e869c7d54cbe38f780abfeaf8bf9670a
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-31 04:53:45 +0300

    refactor: Update MissionSection component styling and remove unused code

--- app/components/tabs/MissionSection.tsx
+++ app/components/tabs/MissionSection.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import MissionCard from "../missions/MissionCard";
 
 type MissionSectionProps = {
-  title: string;
+  title?: string;
 };
 
 const MissionSection: React.FC<MissionSectionProps> = ({ title }) => {
   return (
-    <div className="no-visible-scrollbar flex flex-col py-4 overflow-y-auto border border-blue-400 bg-blue-400/30 rounded-xl h-[90%] w-1/2 items-start justify-start">
+    <div className="no-visible-scrollbar flex flex-col overflow-y-auto  mb-6 rounded-xl h-[80%] w-1/2 items-start justify-start">
       <p className="text-center w-full">{title}</p>
       <div className="gap-4  flex  flex-col p-4 w-full">
         {mockMissionData.map((mission, index) => (
