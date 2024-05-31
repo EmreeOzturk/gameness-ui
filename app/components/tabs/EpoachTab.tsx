2ef6a070f30dcffa46b4cb8678a76e3fb8f0d30a
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-31 03:25:34 +0300

    refactor: Update EpoachTab component layout and styling

--- app/components/tabs/EpoachTab.tsx
+++ app/components/tabs/EpoachTab.tsx
@@ -12,12 +12,12 @@ const EpoachTab: React.FC<EpoachTabProps> = ({
   points,
 }) => {
   return (
-    <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-zinc-900 to-zinc-950">
+    <div className="w-full select-none overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-t from-sky-700 via-sky-950 to-slate-950">
       <p>{title}</p>
-      <div className="flex items-center h-full gap-4 justify-between">
+      {/* <div className="flex items-center h-full gap-4 justify-between">
         <MissionSection title="Daily Missions" />
         <MissionSection title="Weekly Missions" />
-      </div>
+      </div> */}
     </div>
   );
 };
