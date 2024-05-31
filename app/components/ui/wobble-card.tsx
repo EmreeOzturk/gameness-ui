a8dd7a1a694e0114a6779b454f2593089a6476b5
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-31 03:25:31 +0300

    refactor: Update WobbleCard component background gradient and box shadow

--- app/components/ui/wobble-card.tsx
+++ app/components/ui/wobble-card.tsx
@@ -41,7 +41,7 @@ export const WobbleCard = ({
       )}
     >
       <div
-        className="relative  h-full [background-image:radial-gradient(60%_70%_at_top,rgba(25,25,255,0.3),rgba(0,0,0,0))]  sm:mx-0 sm:rounded-2xl overflow-hidden"
+        className="relative  h-full [background-image:radial-gradient(60%_70%_at_top,rgb(0,110,255,.7),rgba(0,0,0,0))]  sm:mx-0 sm:rounded-2xl overflow-hidden"
         style={{
           boxShadow:
             "0 10px 32px rgba(34, 42, 53, 0.12), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.05), 0 4px 6px rgba(34, 42, 53, 0.08), 0 24px 108px rgba(47, 48, 55, 0.10)",
