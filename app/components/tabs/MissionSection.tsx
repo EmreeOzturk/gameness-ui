0b3d1a6541c356148f8bcf3eedcea2415ccfd9d0
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-31 07:09:25 +0300

    refactor: Update MissionSection to use dynamic data for MissionCard rendering

--- app/components/tabs/MissionSection.tsx
+++ app/components/tabs/MissionSection.tsx
@@ -1,17 +1,18 @@
-import mockMissionData from "@/app/data/missions";
+import { MissionCardProps } from "@/app/data/missions";
 import React from "react";
 import MissionCard from "../missions/MissionCard";
 
 type MissionSectionProps = {
   title?: string;
+  data?: MissionCardProps[];
 };
 
-const MissionSection: React.FC<MissionSectionProps> = ({ title }) => {
+const MissionSection: React.FC<MissionSectionProps> = ({ title, data }) => {
   return (
     <div className="no-visible-scrollbar flex flex-col overflow-y-auto  mb-6 rounded-xl h-[80%] w-1/2 items-start justify-start">
       <p className="text-center w-full">{title}</p>
       <div className="gap-4  flex  flex-col p-4 w-full">
-        {mockMissionData.map((mission, index) => (
+        {data?.map((mission, index) => (
           <MissionCard
             key={index}
             title={mission.title}
