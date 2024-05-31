310f544cd468756b8e32a6a00430fa07cacfb8d5
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-31 03:25:43 +0300

    refactor: Adjust height and styling of MovingBorder component

--- app/components/ui/moving-border.tsx
+++ app/components/ui/moving-border.tsx
@@ -47,7 +47,7 @@ export function Button({
         <MovingBorder duration={duration} rx="30%" ry="30%">
           <div
             className={cn(
-              "h-20 w-20 opacity-[0.8] bg-[radial-gradient(var(--sky-500)_40%,transparent_60%)]",
+              "h-24 w-24 opacity-[0.5] bg-[radial-gradient(var(--sky-600)_40%,transparent_60%)]",
               borderClassName
             )}
           />
@@ -56,7 +56,7 @@ export function Button({
 
       <div
         className={cn(
-          "relative bg-slate-900/[0.8] border border-slate-800 backdrop-blur-xl text-white flex items-center justify-center w-full h-full text-sm antialiased",
+          "relative bg-slate-900/[0.8]   backdrop-blur-xl text-white flex items-center justify-center w-full h-full text-sm antialiased",
           className
         )}
         style={{
@@ -71,7 +71,7 @@ export function Button({
 
 export const MovingBorder = ({
   children,
-  duration = 2000,
+  duration = 2400,
   rx,
   ry,
   ...otherProps
