d2c1ef00bb0a728fbe8b833a2c50b2d1a461c413
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-31 06:44:40 +0300

    refactor: Update EpoachTab background gradient colors

--- app/components/tabs/EpoachTab.tsx
+++ app/components/tabs/EpoachTab.tsx
@@ -12,7 +12,7 @@ const EpoachTab: React.FC<EpoachTabProps> = ({
   points,
 }) => {
   return (
-    <div className="w-full select-none text-zinc-300 overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold  bg-gradient-to-t from-sky-700 via-sky-950 to-slate-950">
+    <div className="w-full select-none text-zinc-300 overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold  bg-gradient-to-t from-sky-800 via-sky-950 to-slate-900">
       <p>{title}</p>
       <div className="flex justify-between w-4/5 gap-10 text-3xl mt-6 items-center  md:px-6 mx-auto">
         <p>Daily Missions (4/10)</p>
