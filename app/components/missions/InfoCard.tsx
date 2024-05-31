3da71216fede4048cb2995ae26f3389db556a5cb
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-31 03:25:20 +0300

    refactor: Update InfoCard component to include icon prop and button styling

--- app/components/missions/InfoCard.tsx
+++ app/components/missions/InfoCard.tsx
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import { Button } from "../ui/moving-border";
 
 type InfoCardProps = {
   title: string;
   data: number;
+  icon?: ReactNode;
 };
-
-const InfoCard: React.FC<InfoCardProps> = ({ title, data }) => {
+const InfoCard: React.FC<InfoCardProps> = ({ title, data, icon }) => {
   return (
     <div
-      style={{
-        border: "0 solid #33020285",
-        width: "0",
-        height: "0",
-        borderLeft: "100px solid transparent",
-        borderRight: "100px solid transparent",
-        borderTop: "170px solid #0485e85d",
-        borderBottom: "0",
-      }}
+      // style={{
+      //   border: "0 solid #33020285",
+      //   width: "0",
+      //   height: "0",
+      //   borderLeft: "100px solid transparent",
+      //   borderRight: "100px solid transparent",
+      //   borderTop: "170px solid #045686e0",
+      //   borderBottom: "0",
+      // }}
       className="relative"
     >
-      <div className="absolute text-center select-none left-[50%] -translate-x-[50%] font-bold text-xl -top-[50%] -translate-y-[165%]">
+      {/* <div className="absolute text-center text-zinc-300 select-none left-[50%] -translate-x-[50%] font-bold text-xl -top-[50%] -translate-y-[165%]">
         <h3 className="mt-2">
           {title.split(" ").map((word, index) => (
             <span key={index} className="block">
@@ -30,6 +31,23 @@ const InfoCard: React.FC<InfoCardProps> = ({ title, data }) => {
         <p className="mt-2">
           <span className="text-xl font-bold">{data}</span>
         </p>
+      </div> */}
+      <div className="select-none  tracking-wide">
+        <Button className="text-zinc-300 px-8 py-4 text-2xl flex flex-col w-64 ">
+          <div>
+            <div className="flex justify-center mb-2">{icon && icon}</div>
+            <h3>
+              {title.split(" ").map((word, index) => (
+                <span key={index} className="block">
+                  {word}
+                </span>
+              ))}
+            </h3>
+          </div>
+          <p className="mt-2">
+            <span className="text-xl font-bold">{data}</span>
+          </p>
+        </Button>
       </div>
     </div>
   );
