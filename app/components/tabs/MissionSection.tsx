646934dab13d0bf08cb54a05d4109fbebc67f364
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-31 03:25:24 +0300

    refactor: Update MissionSection component layout and styling

--- app/components/tabs/MissionSection.tsx
+++ app/components/tabs/MissionSection.tsx
@@ -8,7 +8,7 @@ type MissionSectionProps = {
 
 const MissionSection: React.FC<MissionSectionProps> = ({ title }) => {
   return (
-    <div className="no-visible-scrollbar flex flex-col py-4 overflow-y-auto border rounded-xl h-[90%] w-1/2 items-start justify-start">
+    <div className="no-visible-scrollbar flex flex-col py-4 overflow-y-auto border border-blue-400 bg-blue-400/30 rounded-xl h-[90%] w-1/2 items-start justify-start">
       <p className="text-center w-full">{title}</p>
       <div className="gap-4  flex  flex-col p-4 w-full">
         {mockMissionData.map((mission, index) => (
