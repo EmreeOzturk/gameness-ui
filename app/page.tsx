759f5f19af731bca27f4a8630f8069882f595c6a
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-01 06:12:50 +0300

    refactor: Add Header component to Home page for better navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import HeroSection from "./components/hero";
 import { BackgroundBeams } from "./components/ui/background-beams";
+import Header from "./components/header";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center">
+    <main className="flex select-none flex-col items-center overflow-hidden justify-center">
+      <div className="pt-8  z-40">
+        <Header />
+      </div>
       <HeroSection />
       <BackgroundBeams />
     </main>
