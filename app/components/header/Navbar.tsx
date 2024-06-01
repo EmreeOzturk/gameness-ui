7e5bd0767286f867df7a95dc04f60e743b5717e1
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-01 03:07:49 +0300

    refactor: Update Navbar component layout for better responsiveness

--- app/components/header/Navbar.tsx
+++ app/components/header/Navbar.tsx
@@ -26,7 +26,7 @@ const navItems = [
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-center gap-4 z-20 ">
+    <div className="flex  items-center justify-center gap-4 z-20 ">
       {navItems.map((item) => (
         <Link
           className="text-zinc-300 flex items-center justify-center gap-2 text-md rounded-xl px-2 transition-all py-2 hover:text-cyan-300"
