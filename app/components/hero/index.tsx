434c10483749d886325154a4bcabdaed21acf539
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-01 05:22:34 +0300

    refactor: Improve responsiveness of HeroSection component

--- app/components/hero/index.tsx
+++ app/components/hero/index.tsx
@@ -5,18 +5,18 @@ import { TextGenerateEffect } from "../ui/text-generate-effect";
 const words = ` Claim, compete, conquer your DATS Project’s XPs!`;
 const HeroSection = () => {
   return (
-    <div className="flex w-full flex-col gap-8  overflow-hidden lg:flex-row items-center justify-center h-screen ">
+    <div className="flex w-full flex-col-reverse  gap-8  lg:pt-0 overflow-hidden lg:flex-row items-center justify-center h-screen ">
       <div className=" z-20  h-full lg:w-1/2 items-start  justify-center flex flex-col gap-4">
         <div className="flex flex-col items-center justify-center gap-4 w-full">
           <TextGenerateEffect
             className="text-2xl w-96 lg:text-xl text-center  font-normal text-slate-300"
             words={words}
           />
-          <div className="bg-gradient-to-br text-center from-cyan-800 to-zinc-300 text-clip bg-clip-text text-transparent text-5xl lg:text-7xl font-bold">
+          <div className="bg-gradient-to-br text-center from-cyan-800 to-zinc-300 text-clip bg-clip-text text-transparent text-6xl my-2 lg:text-7xl font-bold">
             <h1>Community Campaign</h1>
           </div>
         </div>
-        <div className="w-full h-1.5 bg-gradient-to-r from-transparent via-sky-500 to-transparent" />
+        <div className="w-4/5 lg:w-full mx-auto h-1.5 bg-gradient-to-r from-transparent via-sky-500 to-transparent" />
         <Link
           href="/missions"
           className="px-4 py-2 rounded-full relative mt-4 lg:mt-10 bg-transparent mx-auto text-zinc-300 text-lg font-bold hover:shadow-md w-1/2 hover:shadow-white/[0.4] transition duration-200 border border-slate-600"
@@ -29,7 +29,7 @@ const HeroSection = () => {
           </div>
         </Link>
       </div>
-      <div className="relative z-10 w-full   lg:w-1/2 h-full  ">
+      <div className="relative z-10 w-full hidden lg:flex  lg:w-1/2 h-full  ">
         <Image
           src="/beyaz2.png"
           alt="Hero"
