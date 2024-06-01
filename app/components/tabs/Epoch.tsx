5566842db85bfc376be8fcc474b08209f8eb3b09
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-01 05:22:37 +0300

    refactor: Update Epoch tab layout for better responsiveness

--- app/components/tabs/Epoch.tsx
+++ app/components/tabs/Epoch.tsx
@@ -17,24 +17,30 @@ const Epoch: React.FC<EpochTabProps> = ({
   dailyTasks,
 }) => {
   return (
-    <div className="w-full select-none text-zinc-300 overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold  bg-gradient-to-t from-slate-950 via-slate-900 to-[#0282C5]">
-      <p>{title}</p>
-      <div className="flex justify-between w-4/5 gap-10 text-3xl mt-6 items-center  md:px-6 mx-auto">
+    <div className="w-full select-none text-zinc-300 overflow-hidden relative h-full rounded-2xl p-4 lg:p-10  font-bold  bg-gradient-to-t from-slate-950 via-slate-900 to-[#0282C5]">
+      <p className="text-3xl lg:text-4xl">{title}</p>
+      <div className="flex flex-col lg:flex-row justify-between text-center  lg:w-4/5 gap-10 lg:text-3xl mt-6 items-center  md:px-6 mx-auto">
         <p>
           Weekly Missions{" "}
           {weeklyTasks.length === 0
             ? "(0/0)"
             : `(${weeklyTasks.length}/${weeklyTasks.length})`}
         </p>
-        <p>
+        <p className="hidden lg:flex">
           Daily Missions{" "}
           {dailyTasks.length === 0
             ? "(0/0)"
             : `(${dailyTasks.length}/${dailyTasks.length})`}
         </p>
       </div>
-      <div className="flex items-start mt-4  gap-4 h-full justify-between">
+      <div className="flex flex-col lg:flex-row items-start mt-4  gap-4 h-full justify-between">
         <MissionSection data={weeklyTasks} />
+        <p className="flex lg:hidden mx-auto">
+          Daily Missions{" "}
+          {dailyTasks.length === 0
+            ? "(0/0)"
+            : `(${dailyTasks.length}/${dailyTasks.length})`}
+        </p>
         <MissionSection data={dailyTasks} />
       </div>
     </div>
