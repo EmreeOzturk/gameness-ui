61293192c91464733bbd2d8485f9855a7537b450
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-02 07:28:54 +0300

    refactor: Update InfoCard component layout for better responsiveness

--- app/components/missions/InfoCard.tsx
+++ app/components/missions/InfoCard.tsx
@@ -9,44 +9,15 @@ type InfoCardProps = {
 };
 const InfoCard: React.FC<InfoCardProps> = ({ title, data, icon, reverse }) => {
   return (
-    <div
-      // style={{
-      //   border: "0 solid #33020285",
-      //   width: "0",
-      //   height: "0",
-      //   borderLeft: "100px solid transparent",
-      //   borderRight: "100px solid transparent",
-      //   borderTop: "170px solid #045686e0",
-      //   borderBottom: "0",
-      // }}
-      className="relative"
-    >
-      {/* <div className="absolute text-center text-zinc-300 select-none left-[50%] -translate-x-[50%] font-bold text-xl -top-[50%] -translate-y-[165%]">
-        <h3 className="mt-2">
-          {title.split(" ").map((word, index) => (
-            <span key={index} className="block">
-              {word}
-            </span>
-          ))}
-        </h3>
-        <p className="mt-2">
-          <span className="text-xl font-bold">{data}</span>
-        </p>
-      </div> */}
-      <div className="select-none  tracking-wide">
+    <div className="relative">
+      <div className="select-none tracking-wide">
         <Button
           reverse={reverse}
           className="text-zinc-300 px-8 py-4 text-base lg:text-xl flex flex-col w-64 "
         >
-          <div>
+          <div className="w-full">
             <div className="flex justify-center mb-2">{icon && icon}</div>
-            <h3>
-              {title.split(" ").map((word, index) => (
-                <span key={index} className="block">
-                  {word}
-                </span>
-              ))}
-            </h3>
+            <h3>{title}</h3>
           </div>
           <p className="mt-2">
             <span className="text-xl font-bold">{data}</span>
