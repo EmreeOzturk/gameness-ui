9970b13f624b05789e78da11086db0210f8aa51d
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-05 20:50:30 +0300

    refactor: Update auth.ts to use Twitter provider from next-auth

--- auth.ts
+++ auth.ts
@@ -1,29 +1,88 @@
 import NextAuth, { type DefaultSession } from "next-auth";
 import Twitter from "next-auth/providers/twitter";
-declare module "next-auth" {
-  interface Session {
-    user: {
-      id: number;
-      name: string;
-      image: string;
-      twitterId: string;
-    };
-  }
-}
+import type { TwitterProfile } from "next-auth/providers/twitter";
+import type { ProfileCallback } from "next-auth/providers";
+import clientPromise from "./app/lib/db";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import { ObjectId } from "mongodb";
+// declare module "next-auth" {
+//   interface Session {
+//     user: {
+//       id: number;
+//       name: string;
+//       image: string;
+//       twitter: string;
+//       refId: string;
+//       screenName: string;
+//       discord: string;
+//       telegram: string;
+//       walletAddress: string;
+//       totalPoints: number;
+//       finishedMissions: string[];
+//       refList: string[];
+//       refPoints: number;
+//       refMultiplier: number;
+//     };
+//   }
+// }
+
+//  refId: string;
+//     screenName?: string;
+//     twitter: string;
+//     discord: string;
+//     telegram: string;
+//     walletAddress: string;
+//     totalPoints: number;
+//     finishedMissions: string[];
+//     refList: string[];
+//     refPoints: number;
+//     refMultiplier: number;
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
   debug: true,
-  providers: [Twitter],
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    Twitter({
+      clientId: process.env.AUTH_TWITTER_ID,
+      clientSecret: process.env.AUTH_TWITTER_SECRET,
+      profile(profile: TwitterProfile) {
+        return {
+          ...profile.data,
+          refId: profile?.data?.username,
+          twitter: profile.data.id,
+          screenName: profile?.data?.username,
+          discord: "",
+          telegram: "",
+          walletAddress: "",
+          totalPoints: 0,
+          finishedMissions: [],
+          refList: [],
+          refPoints: 0,
+          refMultiplier: 0,
+        };
+      },
+    }),
+  ],
   callbacks: {
-    jwt({ token, account }) {
-      if (account) {
-        token.twitterId = account?.providerAccountId;
-      }
-      return token;
-    },
-    session({ session, token }) {
-      session.user.twitterId = token.twitterId as string;
-      return session;
+    async session({ session, token }) {
+      const user = await (
+        await clientPromise
+      )
+        .db("test")
+        .collection("users")
+        .findOne({
+          _id: new ObjectId(token.sub),
+        });
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          ...user,
+        },
+      };
     },
   },
 });
