09448d6f2ba9913860fcd54c38e9822c1f486eef
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-05 20:52:35 +0300

    refactor: Update MissionSection component to use TaskSchema and add session-based functionality

--- app/components/tabs/MissionSection.tsx
+++ app/components/tabs/MissionSection.tsx
@@ -1,23 +1,36 @@
-import { MissionCardProps } from "@/app/data/missions";
+import { TaskSchema } from "@/app/data/missions";
 import React from "react";
 import MissionCard from "../missions/MissionCard";
+import { auth } from "@/auth";
+import { Session } from "next-auth";
 
 type MissionSectionProps = {
   title?: string;
-  data?: MissionCardProps[];
+  data?: TaskSchema[];
 };
 
-const MissionSection: React.FC<MissionSectionProps> = ({ title, data }) => {
+const MissionSection: React.FC<MissionSectionProps> = async ({
+  title,
+  data,
+}) => {
+  const session = (await auth()) as Session;
   return (
     <div className="no-visible-scrollbar flex flex-col   mb-6 rounded-xl h-full w-full lg:w-1/2 bgitems-start justify-start">
       <p className="text-center w-full">{title}</p>
       <div className="gap-4  flex flex-col lg:p-4 w-full">
         {data?.map((mission, index) => (
           <MissionCard
-            key={index}
-            title={mission.title}
-            description={mission.description}
-            points={mission.points}
+            key={mission._id}
+            _id={mission._id}
+            mission_description={mission.mission_description}
+            mission_point={mission.mission_point}
+            mission_title={mission.mission_title}
+            mission_type={mission.mission_type}
+            mission_joiners={mission.mission_joiners}
+            mission_link={mission.mission_link}
+            mobile_mission_link={mission.mobile_mission_link}
+            isDone={session.user?.finishedMissions.includes(mission._id)}
+            userId={session.user?._id}
           />
         ))}
       </div>
