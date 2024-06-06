5e66f6a1c76c79a7d06b29195c5a5c819cff8c45
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-07 02:44:40 +0300

    refactor: Update HeroSection component with separate CTASection and HeroImage components

--- app/components/hero/index.tsx
+++ app/components/hero/index.tsx
@@ -1,41 +1,10 @@
-import Image from "next/image";
-import Xicon from "../icons/Xicon";
-import { TextGenerateEffect } from "../ui/text-generate-effect";
-import signWithX from "@/app/action";
-const words = ` Claim, compete, conquer your DATS Project’s XPs!`;
+import CTASection from "./CTASection";
+import HeroImage from "./HeroImage";
 const HeroSection = () => {
   return (
     <div className="flex w-full flex-col-reverse -mt-20 md:mt-0 gap-8  md:pt-0 overflow-hidden md:flex-row items-center justify-center h-screen ">
-      <div className=" z-20  h-full lg:w-1/2 items-start  justify-center flex flex-col gap-4">
-        <div className="flex flex-col items-center justify-center gap-4 w-full">
-          <TextGenerateEffect
-            className="text-2xl w-96 lg:text-xl text-center  font-normal text-slate-300"
-            words={words}
-          />
-          <div className="bg-gradient-to-br text-center from-cyan-800 to-zinc-300 text-clip bg-clip-text text-transparent text-4xl my-2 lg:text-7xl font-bold">
-            <h1>Community Campaign</h1>
-          </div>
-        </div>
-        <div className="w-4/5 lg:w-full mx-auto h-1.5 bg-gradient-to-r from-transparent via-sky-500 to-transparent" />
-        <div className="px-4 py-2 rounded-full relative mt-4 lg:mt-6 bg-transparent mx-auto text-zinc-300 text-lg font-bold hover:shadow-md w-1/2 hover:shadow-white/[0.4] transition duration-200 border border-slate-600">
-          <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-xl  bg-gradient-to-r from-transparent via-sky-500 to-transparent" />
-          <div className="relative  flex justify-center items-center gap-2 z-20">
-            <form action={signWithX}>
-              <button className="flex items-center gap-1" type="submit">
-                Sign in with <Xicon />
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-      <div className="relative z-10 w-full hidden lg:flex  lg:w-1/2 h-full  ">
-        <Image
-          src="/beyaz2.png"
-          alt="Hero"
-          className="object-contain bg-transparent  h-full"
-          fill
-        />
-      </div>
+      <CTASection />
+      <HeroImage />
     </div>
   );
 };
