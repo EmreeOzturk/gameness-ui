807b45b7c356ef6603e92d0f4b74965737be906c
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-07 02:44:32 +0300

    refactor: Optimize image loading in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Header from "./components/header";
 export default function Home() {
   return (
     <main className="flex select-none flex-col items-center max-h-screen overflow-hidden justify-center">
-      <div className="pt-8  z-40">
+      <div className="pt-8 z-40">
         <Header />
       </div>
       <HeroSection />
