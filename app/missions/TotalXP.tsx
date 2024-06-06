b5146e60efb3b75cd6cf6d5cac31c42edd80d68e
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-07 02:58:14 +0300

    refactor: Add TotalXP component to display total XP on the homepage

---
+++ app/missions/TotalXP.tsx
@@ -0,0 +1,9 @@
+const TotalXP = () => {
+  return (
+    <div className="bg-gradient-to-br text-center from-cyan-800 to-zinc-300 text-clip bg-clip-text text-transparent text-5xl lg:text-6xl font-bold">
+      <h1 className="tracking-wide">Total XP: 999</h1>
+    </div>
+  );
+};
+
+export default TotalXP;
