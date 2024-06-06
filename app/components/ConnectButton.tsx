2f67a725f827d688c216fa291f390a714f3b9e66
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-07 01:33:35 +0300

    feat: Add ConnectButton component for connecting wallets

---
+++ app/components/ConnectButton.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { useAccount } from "wagmi";
+import { motion } from "framer-motion";
+import { formatAddress } from "../lib/utils";
+
+
+const ConnectButton = () => {
+  const { open } = useWeb3Modal();
+  const { address: account, isConnected } = useAccount();
+
+  const buttonVariants = {
+    initial: { scale: 1 },
+    whileTap: { scale: 0.99 },
+  };
+
+  return (
+    <div className="flex items-center gap-2">
+      <motion.button
+        onClick={() => open()}
+        className={`py-3 w-full text-white/90 rounded-full font-primary text-sm ${
+          isConnected ? "bg-primary/80" : "bg-primary"
+        }`}
+        variants={buttonVariants}
+        initial="initial"
+        whileTap="whileTap"
+      >
+        {account ? formatAddress(account) : "Connect Wallet"}
+      </motion.button>
+    </div>
+  );
+};
+
+export default ConnectButton;
