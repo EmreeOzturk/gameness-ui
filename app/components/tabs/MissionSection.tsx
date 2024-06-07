cd1e8d1be6b0aec3fc18639e9c2ec40cb867e1ca
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-07 23:21:11 +0300

    refactor: Update MissionSection component to fix rendering issue and improve code readability

--- app/components/tabs/MissionSection.tsx
+++ app/components/tabs/MissionSection.tsx
@@ -18,21 +18,24 @@ const MissionSection: React.FC<MissionSectionProps> = async ({
     <div className="no-visible-scrollbar flex flex-col   mb-6 rounded-xl h-full w-full lg:w-1/2 bgitems-start justify-start">
       <p className="text-center w-full">{title}</p>
       <div className="gap-4  flex flex-col lg:p-4 w-full">
-        {data?.map((mission, index) => (
-          <MissionCard
-            key={mission._id}
-            _id={mission._id}
-            mission_description={mission.mission_description}
-            mission_point={mission.mission_point}
-            mission_title={mission.mission_title}
-            mission_type={mission.mission_type}
-            mission_joiners={mission.mission_joiners}
-            mission_link={mission.mission_link}
-            mobile_mission_link={mission.mobile_mission_link}
-            isDone={session.user?.finishedMissions.includes(mission._id)}
-            userId={session.user?._id}
-          />
-        ))}
+        {data?.map((mission, index) => {
+          console.log(session.user?.finishedMissions.includes(mission._id));
+          return (
+            <MissionCard
+              key={mission._id}
+              _id={mission._id}
+              mission_description={mission.mission_description}
+              mission_point={mission.mission_point}
+              mission_title={mission.mission_title}
+              mission_type={mission.mission_type}
+              mission_joiners={mission.mission_joiners}
+              mission_link={mission.mission_link}
+              mobile_mission_link={mission.mobile_mission_link}
+              isDone={session.user?.finishedMissions.includes(mission._id)}
+              userId={session.user?._id}
+            />
+          );
+        })}
       </div>
     </div>
   );
