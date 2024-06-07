8f32b3d7d7979420b201799d0df5433c1be0f72a
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-07 23:20:51 +0300

    refactor: Add getTasks and pointUser functions to action.ts

--- app/action.ts
+++ app/action.ts
@@ -1,7 +1,42 @@
 "use server";
-
+import Task from "@/app/models/TaskModel";
+import { revalidatePath } from "next/cache";
+import connectMongo from "@/app/lib/connectMongo";
 import { signIn } from "@/auth";
-
+import { auth } from "@/auth";
 export default async function signWithX() {
   await signIn("twitter", { redirectTo: "/missions" });
+}
+
+export async function getTasks() {
+  try {
+    await connectMongo();
+    const tasks = await Task.find({});
+    console.log(tasks);
+    return tasks;
+  } catch (error) {
+    return { error };
+  }
+}
+export async function pointUser(userID: string, taskID: string) {
+  console.log("pointUser", userID, taskID);
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_BACKEND_URL + "/api/pointUser",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userID, taskID }),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  const data = await response.json();
+  console.log(data);
+  revalidatePath("/missions");
+  return data;
 }
