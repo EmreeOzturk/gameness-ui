1d27e0693b686d9092caecd81e2b1bc3d2843318
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-08 06:08:35 +0300

    refactor: Update InfoCardSection component to fetch assigned resources and display dynamically

--- app/components/missions/InfoCardSection.tsx
+++ app/components/missions/InfoCardSection.tsx
@@ -1,25 +1,47 @@
-import React from "react";
 import InfoCard from "./InfoCard";
 import { Calculator, Gem, Router, Users } from "lucide-react";
-
-const InfoCardSection = () => {
+import { auth } from "@/auth";
+import { Session } from "next-auth";
+const InfoCardSection = async () => {
+  const session = (await auth()) as Session;
+  const refCount = session.user?.refList.length;
+  const resources = await fetch(
+    "https://report.datsproject.io/leader-board/all",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await resources.json();
+  const assignedResource = data.filter(
+    (resource: any) => resource?.address === session.user?.walletAddress
+  );
+  const assignedCPU = assignedResource[0]?.assignedCpuCount;
+  const assignedBandwidth = assignedResource[0]?.assignedBandwith;
+  const assignedResourceCount = assignedResource[0]?.assignedResourceCount;
   return (
     <div className="flex flex-wrap   lg:flex-row gap-3 md:gap-4 items-center px-16 py-8 w-full justify-center ">
       <InfoCard
         title={"Assigned Resource"}
-        data={10}
+        data={assignedResourceCount ? assignedResourceCount : 0}
         icon={<Calculator size={24} />}
       />
-      <InfoCard title={"Assigned CPU"} data={100} icon={<Gem size={24} />} />
+      <InfoCard
+        title={"Assigned CPU"}
+        data={assignedCPU ? assignedCPU : 0}
+        icon={<Gem size={24} />}
+      />
       <InfoCard
         title={"Assigned Bandwidth"}
-        data={20}
+        data={assignedBandwidth ? assignedBandwidth : 0}
         icon={<Router size={24} />}
         reverse
       />
       <InfoCard
         title={"Total Refferals"}
-        data={4}
+        data={refCount}
         icon={<Users size={24} />}
         reverse
       />
