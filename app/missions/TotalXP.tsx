c3ce87c34da04e15a1a2113cd9fdbc3e4e798b82
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-08 06:08:16 +0300

    refactor: Update TotalXP component to display user's total XP dynamically

--- app/missions/TotalXP.tsx
+++ app/missions/TotalXP.tsx
@@ -1,7 +1,14 @@
-const TotalXP = () => {
+import { auth } from "@/auth";
+import { Session } from "next-auth";
+
+const TotalXP = async () => {
+  const session = (await auth()) as Session;
   return (
     <div className="bg-gradient-to-br text-center from-cyan-800 to-zinc-300 text-clip bg-clip-text text-transparent text-5xl lg:text-6xl font-bold">
-      <h1 className="tracking-wide">Total XP: 999</h1>
+      <h1 className="tracking-wide">
+        Total XP:
+        {session.user?.totalPoints}
+      </h1>
     </div>
   );
 };
