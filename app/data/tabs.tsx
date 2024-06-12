3c28ae8a93d266f3ff504fd8cc6e9d0924a5df6e
Emre Ozturk <emreeozturk42@gmail.com>
2024-06-12 08:52:26 +0300

    refactor: Update tab descriptions in tabs.tsx

--- app/data/tabs.tsx
+++ app/data/tabs.tsx
@@ -1,15 +1,5 @@
 import Epoch from "../components/tabs/Epoch";
-// import {
-//   TaskSchema,
-//   week1DailyTasks,
-//   week1WeeklyTasks,
-//   week2DailyTasks,
-//   week2WeeklyTasks,
-//   week3DailyTasks,
-//   week3WeeklyTasks,
-//   week4DailyTasks,
-//   week4WeeklyTasks,
-// } from "@/app/data/missions";
+
 
 export const tabs = [
   {
@@ -20,8 +10,6 @@ export const tabs = [
         title="Epoch 1"
         value="epoch1"
         description="Epoch 1 Description"
-        // dailyTasks={week1DailyTasks}
-        // weeklyTasks={week1WeeklyTasks}
       />
     ),
   },
@@ -33,8 +21,6 @@ export const tabs = [
         title="Epoch 2"
         value="epoch2"
         description="Epoch 2 Description"
-        // dailyTasks={week2DailyTasks}
-        // weeklyTasks={week2WeeklyTasks}
       />
     ),
     // disabled: true,
@@ -47,8 +33,6 @@ export const tabs = [
         title="Epoch 3"
         value="epoch3"
         description="Epoch 3 Description"
-        // dailyTasks={week3DailyTasks}
-        // weeklyTasks={week3WeeklyTasks}
       />
     ),
     // disabled: true,
@@ -61,8 +45,6 @@ export const tabs = [
         title="Epoch 4"
         value="epoch4"
         description="Epoch 4 Description"
-        // dailyTasks={week4DailyTasks}
-        // weeklyTasks={week4WeeklyTasks}
       />
     ),
     // disabled: true,
